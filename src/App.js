1e27291906dc24fa80ff5b880806f51a3bcb0a7a
san man <sance1921@gmail.com>
2022-03-24 20:29:53 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import Register from './pages/Register';
+import ProductList from './pages/ProductList';
 
 function App() {
   return (
-    <div>
-      {/* <Home>
-        <Route path="/" element={<Home />} />
-        <Route path="/pay" element={<Pay />} />
-        <Route path="/succes" element={<Succes />} />
-        {/* <Route path="/" element= {<Home />} />   */}
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
